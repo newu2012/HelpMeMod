04ffa977166ef6fd94a1bfa8369913bd9ccd23d1
newu2012 <newu2011@gmail.com>
2022-02-16 02:12:21 +0500

    add simple header, remove unused code

---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      title: "HelpMeMod",
+    };
+  },
+});
+</script>
+
+<template>
+  <header>
+    <div class="brand">
+      <img alt="Vue logo" class="logo" src="../assets/logo.svg" />
+      <p class="navBarText">{{ title }}</p>
+    </div>
+  </header>
+</template>
+
+<style>
+header {
+  position: fixed;
+  height: 64px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: darkslategray;
+}
+
+.brand {
+  display: inline-flex;
+  flex-direction: row;
+  gap: 8px;
+  padding: 8px;
+  margin: auto;
+}
+
+.brand:hover {
+  cursor: pointer;
+}
+
+.logo {
+  width: 32px;
+}
+
+.navBarText {
+  color: white;
+}
+</style>
