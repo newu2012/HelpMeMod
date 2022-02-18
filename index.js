6735dabd467a8d03575010c0cfad83ab90c7dc8a
newu2012 <newu2011@gmail.com>
2022-02-18 05:24:51 +0500

    add connect-history-api-fallback, reformat

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
+const history = require("connect-history-api-fallback");
 const app = express(),
   bodyParser = require("body-parser"),
   port = 8080;
 
-const vueAppPath = '/Vue/dist'
+const vueAppPath = "/Vue/dist";
 
+app.use(history());
 app.use(bodyParser.json());
 app.use(express.static(process.cwd() + vueAppPath));
 
-app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + `${vueAppPath}/index.html`)
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + `${vueAppPath}/index.html`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
