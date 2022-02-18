db53e116218b1e2704bbb394d82c1c0c2e920673
newu2012 <newu2011@gmail.com>
2022-02-18 19:02:08 +0500

    add AboutPage, EmptyPage and HomePage pages

---
+++ Vue/src/pages/AboutPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavigationHeader from "../components/NavigationHeader.vue";
+</script>
+
+<template>
+  <NavigationHeader />
+  <div>
+    <p>Info about this site</p>
+  </div>
+</template>
+
+<style></style>
