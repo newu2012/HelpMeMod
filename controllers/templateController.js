b00c4a85fd7fc207408d4283e150cd256fc2f5bf
newu2012 <newu2011@gmail.com>
2022-02-19 18:43:52 +0500

    add About.xml in response on /template

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -1,10 +1,13 @@
 const Template = require("../models/template");
+const path = require("path");
 
 const getTemplate = async (req, res, next) => {
   try {
-    console.log("HELP");
     const templateZip = null; // TODO await get mod_template.zip from Template function
     if (templateZip == null) {
+      //  TODO Fix 
+      res.sendFile(path.join(__dirname, "../mod_template/About/About.xml"));
+      return;
       res.status(404).send("Can't find ready template");
     } else {
       res.send(templateZip);
