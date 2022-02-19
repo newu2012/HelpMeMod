130dbca66da9239f88cb6776822e6f58d2f0e9e9
newu2012 <newu2011@gmail.com>
2022-02-19 22:09:54 +0500

    add button to download mod_template.xml

--- Vue/dist/assets/AboutPage.7e773cbc.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./NavigationHeader.3c4c433a.js";import{e as t,o as a,f as s,i as r,F as n,g as e}from"./vendor.0da7e088.js";import"./index.b7c7fcbd.js";const c=e("div",null,[e("h2",{style:{color:"black"}},"Info about this site")],-1),f=t({setup(i){return(l,m)=>(a(),s(n,null,[r(o),c],64))}});export{f as default};
