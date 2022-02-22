ad8e0c4e2e78efffc2c0605204ace079afcc76f4
newu2012 <newu2011@gmail.com>
2022-02-22 18:06:54 +0500

    remove all tries to build vue on github actions

--- src/fire.ts
+++ src/fire.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { Auth } from "firebase/auth"
 
 import { firebaseConfig } from "./configs/fire.config";
 // TODO: Add SDKs for Firebase products that you want to use
