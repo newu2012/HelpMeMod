60c16d4464e9b7857190f5c2e56b0e1ede1d19bc
newu2012 <newu2011@gmail.com>
2022-02-22 19:25:22 +0500

    try fix zip not downloading on firebase server

--- src/controllers/templateController.ts
+++ src/controllers/templateController.ts
@@ -1,9 +1,10 @@
+import fs from "fs";
 import { Template } from "../models/template";
 
 export const getTemplate = async (req, res, next) => {
   try {
     const templateZipPath = Template.zipTo;
-    if (templateZipPath == null) {
+    if (templateZipPath == null || !fs.existsSync(templateZipPath)) {
       res.status(404).send("Can't find ready template");
     } else {
       res.sendFile(templateZipPath);
